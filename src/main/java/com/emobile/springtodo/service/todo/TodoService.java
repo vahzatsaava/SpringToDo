0b927f2846cbea9f96a7745383ccc99503482c7e
vahzatsaava <vazhatsaava@gmail.com>
2025-01-09 13:52:56 +0400

    09.01.2025

--- src/main/java/com/emobile/springtodo/service/todo/TodoService.java
+++ src/main/java/com/emobile/springtodo/service/todo/TodoService.java
@@ -8,6 +8,7 @@ import java.security.Principal;
 import java.util.List;
 
 public interface TodoService {
+
     void saveTodo(TodoCreateRequest request, Principal principal);
     TodoResponse updateTodo(TodoUpdateRequest request, Principal principal);
     List<TodoResponse> allTodosByPrincipalWithPagination(Principal principal,int page, int size);
